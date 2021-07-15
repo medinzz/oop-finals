3d4a0d0d46aae93bdb1969d682d7c940ff911a1b
joshua merwinm <jmv.medina@iskolarngbayan.pup.edu.ph>
2021-07-16 03:02:31 +0800

    initial

---
+++ main.js
@@ -0,0 +1,27 @@
+
+let typed = new Typed('.typed-text', {
+  strings: [
+    "MARILAO, JUSTIN TROY R.",
+    "FAJARDO, LOUISE KAYE A.",
+    "MAHINAY, JOANNE KAYE F.",
+    "HILARIO, ANDREW G.",
+    "CORPUZ, BITHIAH PELAIAH T",
+    "RIVERA, HANNAH JOY A.",
+    "CORALES, MARK REINELL S.",
+    "LUMABAN, LEMNUEL S.",
+    "MEDINA, JOSHUA MERWIN V.",
+    "LUMAQUE, JHERICOH JANQUILL N.",
+    "CRUZ, ACE VON ALFRED L.",
+    "AYAP, KATHLEEN SHAYNE"
+  ],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 2000,
+  showCursor: false,
+  loop: true
+});
+
+
+$(document).ready( function() {
+  $("#oop").load("components/oop.html");
+});
